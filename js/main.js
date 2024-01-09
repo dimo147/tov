215473ffe6c448c2ee41315a98417e212e071e52
omid jalilian <omidjalilian147@gmail.com>
2024-01-10 02:40:49 +0330

    navigation done

--- js/main.js
+++ js/main.js
@@ -71,3 +71,18 @@ function closeNav() {
     document.body.style.backgroundColor = "white";
     // document.getElementById("main").style.marginRight= "0";
 }
+
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+    this.classList.toggle("cur");
+    var dropdownContent = this.nextElementSibling;
+    if (dropdownContent.style.display === "block") {
+      dropdownContent.style.display = "none";
+    } else {
+      dropdownContent.style.display = "block";
+    }
+  });
+}
